143f2063111bbf7a24d838125502e36ffe204a8d
Ashraf Fouda <ashraf.m.fouda@gmail.com>
2017-05-31 12:30:30 +0200

    Adds fix to ays base url
    Signed-off-by: Ashraf Fouda <ashraf.m.fouda@gmail.com>

--- JumpScale9AYS/ays/server/app.py
+++ JumpScale9AYS/ays/server/app.py
@@ -9,8 +9,8 @@ app = Sanic(__name__)
 app.blueprint(ays_if)
 app.blueprint(webhooks_if)
 
-app.static('/apidocs', 'apidocs')
-app.static('/', 'index.html')
+app.static('/apidocs', 'JumpScale9AYS/ays/server/apidocs/index.html')
+app.static('/', 'JumpScale9AYS/ays/server/index.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000, workers=1)
