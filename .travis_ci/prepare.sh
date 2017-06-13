57c5195ea216854193e9d0042bc1909e36f52175
Abdelrahman Hussein <ahussein@codescalers.com>
2017-06-13 12:24:39 +0200

    dont use js9_start and use the corrent container name

--- .travis_ci/prepare.sh
+++ .travis_ci/prepare.sh
@@ -14,6 +14,3 @@ curl https://raw.githubusercontent.com/Jumpscale/developer/$GIGDEVELOPERBRANCH/j
 # build image
 source ~/.jsenv.sh
 js9_build -l
-
-# start/create the container
-js9_start
--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # start ays
-sudo docker exec js9 js9 'j.atyourservice.server.start()'
+sudo docker exec js9_base js9 'j.atyourservice.server.start()'
 
 # check if the server started
-sudo docker exec js9 js9 'cli=j.clients.atyourservice.get();cli.api.ays.listRepositories()'
+sudo docker exec js9_base js9 'cli=j.clients.atyourservice.get();cli.api.ays.listRepositories()'
