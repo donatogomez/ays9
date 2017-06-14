7e1c2e2534ddb1c30632d228ed241b5dda753b0a
Abdelrahman Hussein <ahussein@codescalers.com>
2017-06-14 13:09:37 +0200

    add sleep between starting the ays server and testing that it is up

--- run_tests.sh
+++ run_tests.sh
@@ -3,5 +3,8 @@
 # start ays
 sudo docker exec js9_base js9 'j.atyourservice.server.start()'
 
+# sleep for 30 seconds
+sleep 30
+
 # check if the server started
 sudo docker exec js9_base js9 'cli=j.clients.atyourservice.get();cli.api.ays.listRepositories()'
